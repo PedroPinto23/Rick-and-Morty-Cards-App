c2681aafe92779b2af3b434917f60de711b36bd3
PedroPinto23 <pdrpdr.ph@gmail.com>
2020-01-30 15:26:32 -0300

    30-01-2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'src/showProfile.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
@@ -7,7 +7,7 @@ import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'wubba lubba dub dub',
+    title: 'RICK AND MORTY CARDS', 
     home: HomeScreen(),
     debugShowCheckedModeBanner: false,
   ));
@@ -124,7 +124,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                       context,
                                       MaterialPageRoute(
                                           builder: (context) =>
-                                              showProfile(data, index)));
+                                              ShowProfile(data, index)));
                                 },
                               ),
                             ),
@@ -217,108 +217,5 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ],
     );
-  }
-
-  // *NAVIGATOR*
-  Widget showProfile(
-    List data,
-    int i,
-  ) {
-    return Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 255, 210, 38),
-          Color.fromARGB(255, 25, 217, 253)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-        padding: EdgeInsets.all(10),
-        child: Card(
-            borderOnForeground: true,
-            color: Colors.yellow,
-            child: Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        data[i]['name'],
-                        style: GoogleFonts.bangers(
-                            color: Colors.blue,
-                            textStyle: TextStyle(fontSize: 42)),
-                      ),
-                      Text(
-                        'origin location: ' + data[i]['origin']['name'],
-                        textAlign: TextAlign.center,
-                        style: GoogleFonts.acme(
-                            color: Colors.blue,
-                            textStyle: TextStyle(fontSize: 22)),
-                      ),
-                      Divider(),
-                      Hero(
-                        tag: data[i]['id'],
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(15),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                border:
-                                    Border.all(color: Colors.black, width: 6)),
-                            child: Image.network(
-                              data[i]['image'],
-                              height: 180,
-                              width: 180,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Divider(),
-                  Text(
-                    'Status: ' + data[i]['status'],
-                    style: GoogleFonts.acme(
-                        color: Colors.blue, textStyle: TextStyle(fontSize: 22)),
-                  ),
-                  Text(
-                    'Species: ' + data[i]['species'],
-                    style: GoogleFonts.acme(
-                        color: Colors.blue, textStyle: TextStyle(fontSize: 22)),
-                  ),
-                  Text(
-                    'Gender: ' + data[i]['gender'],
-                    style: GoogleFonts.acme(
-                        color: Colors.blue, textStyle: TextStyle(fontSize: 22)),
-                  ),
-                  Text(
-                    'Location: ' + data[i]['location']['name'],
-                    style: GoogleFonts.acme(
-                        color: Colors.blue, textStyle: TextStyle(fontSize: 22)),
-                  ),
-                  Divider(),
-                  GestureDetector(
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text(
-                          'Back',
-                          style: GoogleFonts.bangers(
-                              color: Colors.blue,
-                              textStyle: TextStyle(fontSize: 30)),
-                        ),
-                        Icon(
-                          Icons.arrow_back,
-                          size: 35,
-                          color: Colors.blue,
-                        ),
-                      ],
-                    ),
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  )
-                ],
-              ),
-            )));
   }
 }
